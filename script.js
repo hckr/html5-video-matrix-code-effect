8432565f4490a14f51a9026cac570b02c5929b28
Luke <luke@luke.co.pl>
2017-01-03 01:39:29 +0100

    Create function abstracting canvas generation, display only canvas and improve CSS

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-let videoPlayer = document.getElementById('video-player'),
-    localVideoSelector = document.getElementById('local-video-selector'),
-    tempCanvas = document.createElement('canvas'),
-    tempCtx = tempCanvas.getContext('2d'),
-    canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d'),
+let localVideoSelector = document.getElementById('local-video-selector'),
+    canvasContainer = document.getElementById('canvas-container'),
     blockSize = 10,
     availableCharacters = '१२३४५६७८९अरतयपसदगहजकलङषचवबनमआथय़फशधघझखळक्षछभणऒθωερτψυιοπασδφγηςκλζχξωβνμΘΩΨΠΣΔΦΓΗςΛΞЯЫУИПДФЧЙЛЗЦБ',
-    charactersOnCanvas = [];
-
-ctx.font = blockSize + 'px monospace';
+    trailLength = 20,
+    greenChange = Math.floor(255 / (trailLength * 2)),
+    otherChange = Math.floor(255 / (trailLength));;
 
 document.getElementById('select-video').addEventListener('click',
     () => localVideoSelector.click(), false);
@@ -16,87 +12,112 @@ document.getElementById('select-video').addEventListener('click',
 localVideoSelector.addEventListener('change',
     function() {
         let file = this.files[0];
-        if (videoPlayer.canPlayType(file.type) === '') {
+        if (document.createElement('video').canPlayType(file.type) === '') {
             alert(`Unfortunately this browser can't play file of type ${file.type}.`);
             return;
         }
-        videoPlayer.src = URL.createObjectURL(file);
+        canvasContainer.innerHTML = '';
+        let c = createMatrixPlayer(URL.createObjectURL(file));
+        canvasContainer.appendChild(c);
     }, false);
 
-function getRandomCharacter() {
-    let index = Math.floor(Math.random() * availableCharacters.length);
-    return availableCharacters[index];
-}
+function createMatrixPlayer(videoSrc) {
+    let videoPlayer = document.createElement('video'),
+        tempCanvas = document.createElement('canvas'),
+        tempCtx = tempCanvas.getContext('2d'),
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        charactersOnCanvas = [],
+        columns,
+        rows,
+        trailHeads = []; // y position for each subsequent column
 
-function setRandomCharacterAt(pos) {
-    return charactersOnCanvas[pos] = getRandomCharacter();
-}
+    ctx.font = blockSize + 'px monospace';
 
-function getCharacterAt(pos) {
-    return charactersOnCanvas[pos] || setRandomCharacterAt(pos);
-}
+    function getRandomCharacter() {
+        let index = Math.floor(Math.random() * availableCharacters.length);
+        return availableCharacters[index];
+    }
 
-function getPossibleRandomCharacterAt(pos) {
-    return (Math.random() < 0.0001) ?
-           setRandomCharacterAt(pos) :
-           getCharacterAt(pos);
-}
+    function setRandomCharacterAt(pos) {
+        return charactersOnCanvas[pos] = getRandomCharacter();
+    }
 
-let columns = Math.floor(videoPlayer.width / blockSize),
-    rows = Math.floor(videoPlayer.height / blockSize),
-    trailHeads = [], // y position for each subsequent column
-    trailLength = 20,
-    greenChange = Math.floor(255 / (trailLength * 2)),
-    otherChange = Math.floor(255 / (trailLength));
+    function getCharacterAt(pos) {
+        return charactersOnCanvas[pos] || setRandomCharacterAt(pos);
+    }
 
-for (let i = 0; i < columns; ++i) {
-    trailHeads[i] = 0;
-}
+    function getPossibleRandomCharacterAt(pos) {
+        return (Math.random() < 0.001) ?
+               setRandomCharacterAt(pos) :
+               getCharacterAt(pos);
+    }
 
-setInterval(function updateTrailHeadsLoop() {
-    for (let i = 0; i < columns; ++i) {
-        trailHeads[i] += 1;
-        if (trailHeads[i] >= rows + trailLength) {
-            trailHeads[i] = -1 * Math.floor(Math.random() * rows * 40);
+    function getFillStyle(column, row, r, g, b) {
+        let gray = Math.min(r, g, b),
+            distanceFromTrailHead = trailHeads[column] - row,
+            newR = 0,
+            newG = gray,
+            newB = 0;
+        if (distanceFromTrailHead >= 0 && distanceFromTrailHead < trailLength) {
+            newG = 255 - distanceFromTrailHead * greenChange;
+            newR = newB = 255 - distanceFromTrailHead * otherChange;
         }
+        return `rgb(${newR}, ${newG}, ${newB})`;
     }
-}, 33);
 
-function getFillStyle(column, row, r, g, b) {
-    let gray = Math.min(r, g, b),
-        distanceFromTrailHead = trailHeads[column] - row,
-        newR = 0,
-        newG = gray,
-        newB = 0;
-    if (distanceFromTrailHead >= 0 && distanceFromTrailHead < trailLength) {
-        newG = 255 - distanceFromTrailHead * greenChange;
-        newR = newB = 255 - distanceFromTrailHead * otherChange;
+    function randomNewTrailHeadPos() {
+        return -1 * Math.floor(Math.random() * rows * 30);
     }
-    return `rgb(${newR}, ${newG}, ${newB})`;
-}
 
-videoPlayer.addEventListener('play',
-    () => {
-        tempCanvas.width = columns;
-        tempCanvas.height = rows;
+    videoPlayer.addEventListener('play',
+        () => {
+            canvas.width = videoPlayer.clientWidth;
+            canvas.height = videoPlayer.clientHeight;
 
-        requestAnimationFrame(function draw() {
-            tempCtx.drawImage(videoPlayer, 0, 0, tempCanvas.width, tempCanvas.height);
-            let imageData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height).data;
-            ctx.fillStyle = 'rgb(0, 10, 0)';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            let row = -1;
-            for (let i = 0, pos = 0; i < imageData.length; i += 4, ++pos) {
-                let column = pos % tempCanvas.width;
-                if (column == 0) {
-                    row += 1;
+            columns = Math.floor(canvas.width / blockSize);
+            rows = Math.floor(canvas.height / blockSize);
+
+            tempCanvas.width = columns;
+            tempCanvas.height = rows;
+
+            requestAnimationFrame(function draw() {
+                tempCtx.drawImage(videoPlayer, 0, 0, tempCanvas.width, tempCanvas.height);
+                let imageData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height).data;
+                //ctx.fillStyle = 'rgb(0, 10, 0)';
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                let row = -1;
+                for (let i = 0, pos = 0; i < imageData.length; i += 4, ++pos) {
+                    let column = pos % tempCanvas.width;
+                    if (column == 0) {
+                        row += 1;
+                    }
+                    let r = imageData[i],
+                        g = imageData[i+1],
+                        b = imageData[i+2];
+                    ctx.fillStyle = getFillStyle(column, row, r, g, b);
+                    ctx.fillText(getPossibleRandomCharacterAt(pos), column * blockSize, row * blockSize);
                 }
-                let r = imageData[i],
-                    g = imageData[i+1],
-                    b = imageData[i+2];
-                ctx.fillStyle = getFillStyle(column, row, r, g, b);
-                ctx.fillText(getPossibleRandomCharacterAt(pos), column * blockSize, row * blockSize);
+                requestAnimationFrame(draw);
+            });
+
+            for (let i = 0; i < columns; ++i) {
+                trailHeads[i] = randomNewTrailHeadPos();
             }
-            requestAnimationFrame(draw);
-        });
-    }, false);
+
+            setInterval(function updateTrailHeadsLoop() {
+                for (let i = 0; i < columns; ++i) {
+                    trailHeads[i] += 1;
+                    if (trailHeads[i] >= rows + trailLength) {
+                        trailHeads[i] = randomNewTrailHeadPos();
+                    }
+                }
+            }, 20);
+        }, false);
+
+    videoPlayer.src = videoSrc;
+    videoPlayer.autoplay = true;
+    canvasContainer.appendChild(videoPlayer);
+
+    return canvas;
+}
